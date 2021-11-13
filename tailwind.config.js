bee7278d890223af94871eac19f6377b577ce08e
Kennan Fattahillah <10755895+KennFatt@users.noreply.github.com>
2021-11-13 17:33:25 +0700

    Feat: add primary color and font family to my design system.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,19 @@
+const theme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   mode: "jit",
   purge: ["./src/pages/**/*.tsx"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "primary-light": "#678349",
+        "primary-dark": "#1E2F24",
+      },
+      fontFamily: {
+        sans: ["secularone", ...theme.fontFamily.sans],
+      },
+    },
   },
   variants: {
     extend: {},
