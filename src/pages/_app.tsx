4a4031b10598626d89c752e84d656e6fa5fcc489
Kennan Fattahillah <10755895+KennFatt@users.noreply.github.com>
2021-11-23 21:25:57 +0700

    Feat: adding new custom `Document` component.
    - Feat: impl `<Head>` component inside `MyApp` component.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,16 @@
 import type { AppProps } from "next/app";
+import Head from "next/head";
+
 import "../../styles/globals.scss";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Kennan Fattahillah&#39;s Space</title>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 export default MyApp;
