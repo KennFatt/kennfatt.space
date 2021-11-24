c364d8ce935b8158cb9aadad5388ba93b16e1c68
Kennan Fattahillah <10755895+KennFatt@users.noreply.github.com>
2021-11-24 15:40:32 +0700

    Change: add new `px` and `py` props utility to `<CenetrContent>` component.

--- src/components/base/CenterContent/CenterContent.tsx
+++ src/components/base/CenterContent/CenterContent.tsx
@@ -1,18 +1,41 @@
 import type { FC } from "react";
+import clsx from "clsx";
+
+type PaddingVariants = "s" | "m" | "l";
 
 /**
  * Component CenterContent's props.
  */
-export interface ICenterContent {}
+export interface ICenterContent {
+  px?: PaddingVariants;
+  py?: PaddingVariants;
+}
 
 /**
  * Component CenterContent
  *
  * This is a container-like component that will center the `children`
  *  into the center of the viewport. It's meant to be responsive across screen sizes.
+ *
+ * default ~ medium breakpoint
+ * - s => 8px ~ 16px
+ * - m => 16px ~ 32px
+ * - l => 24px ~ 48px
  */
-export const CenterContent: FC<ICenterContent> = ({ children }) => {
+export const CenterContent: FC<ICenterContent> = ({ px, py, children }) => {
+  const paddingClass = clsx({
+    // x-axis
+    "px-2 md:px-4": px === "s",
+    "px-4 md:px-8": px === "m" || !px, // default
+    "px-6 md:px-12": px === "l",
+
+    // y-axis
+    "py-2 md:py-4": py === "s",
+    "py-4 md:py-8": py === "m",
+    "py-6 md:py-12": py === "l",
+  });
+
   return (
-    <div className="max-w-8xl md:px-8 w-full px-4 mx-auto">{children}</div>
+    <div className={`max-w-8xl w-full mx-auto ${paddingClass}`}>{children}</div>
   );
 };
