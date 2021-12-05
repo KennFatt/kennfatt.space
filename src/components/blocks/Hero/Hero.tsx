5945b3e555dde50d3444a4e0906086446a66b012
Kennan Fattahillah <10755895+KennFatt@users.noreply.github.com>
2021-12-05 18:49:59 +0700

    Change: simplify `<Hero>` component by composing it with a section.

--- src/components/blocks/Hero/Hero.tsx
+++ src/components/blocks/Hero/Hero.tsx
@@ -1,6 +1,6 @@
 import type { FC } from "react";
 
-import { CenterContent } from "components/base/CenterContent";
+import { Section } from "../Section";
 
 import styles from "./Hero.module.scss";
 
@@ -15,15 +15,16 @@ export interface IHero {}
  */
 export const Hero: FC<IHero> = () => {
   return (
-    <section className="lg:text-6xl lg:leading-relaxed flex items-center text-3xl leading-relaxed min-h-[calc(100vh-5rem)] md:min-h-[calc(100vh-7.75rem)]">
-      <CenterContent>
-        <h1 className="font-serif">
-          <span className={styles.highlighted}>Kennan Fattahillah</span> is a{" "}
-          <span className={styles["strike-out"]}>Frontend Developer</span>{" "}
-          <span className={styles.highlighted}>Creative Problem Solver</span>{" "}
-          from Jakarta, Indonesia.
-        </h1>
-      </CenterContent>
-    </section>
+    <Section
+      className="lg:text-6xl lg:leading-relaxed flex items-center text-3xl leading-relaxed min-h-[calc(100vh-5rem)] md:min-h-[calc(100vh-7.75rem)]"
+      space={false}
+    >
+      <h1 className="font-serif">
+        <span className={styles.highlighted}>Kennan Fattahillah</span> is a{" "}
+        <span className={styles["strike-out"]}>Frontend Developer</span>{" "}
+        <span className={styles.highlighted}>Creative Problem Solver</span> from
+        Jakarta, Indonesia.
+      </h1>
+    </Section>
   );
 };
