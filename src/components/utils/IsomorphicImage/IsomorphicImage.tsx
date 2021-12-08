99fec709cc086ff5ff720007c115bbb570d439bf
Kennan Fattahillah <10755895+KennFatt@users.noreply.github.com>
2021-12-09 01:29:45 +0700

    Change: simplify solution from 52b6163bbdfae9f3309f3cfd8ba3c9aeed5af79b

--- src/components/utils/IsomorphicImage/IsomorphicImage.tsx
+++ src/components/utils/IsomorphicImage/IsomorphicImage.tsx
@@ -1,42 +1,15 @@
 import type { FC, HTMLAttributes } from "react";
+import type { ImageProps } from "next/image";
 import Image from "next/image";
+
+type ImageOverrideProps = HTMLAttributes<HTMLImageElement> & ImageProps;
 
 /**
  * Component IsomorphicImage's props.
- *
- * Please refer to ImageProps from "next/image" for the type definitions.
  */
-export interface IIsomorphicImage extends HTMLAttributes<HTMLImageElement> {
+export interface IIsomorphicImage extends ImageOverrideProps {
   src: string;
   alt?: string;
-
-  width?: number | string;
-  height?: number | string;
-  layout?: "fill" | "fixed" | "intrinsic" | "responsive" | undefined;
-
-  loader?: (resolverProps: {
-    src: string;
-    width: number;
-    quality?: number;
-  }) => string;
-
-  quality?: number | string;
-  priority?: boolean;
-  loading?: "lazy" | "eager" | undefined;
-  lazyBoundary?: string;
-  placeholder?: "blur" | "empty";
-  blurDataURL?: string;
-  unoptimized?: boolean;
-
-  objectFit?: NonNullable<JSX.IntrinsicElements["img"]["style"]>["objectFit"];
-  objectPosition?: NonNullable<
-    JSX.IntrinsicElements["img"]["style"]
-  >["objectPosition"];
-
-  onLoadingComplete?: (result: {
-    naturalWidth: number;
-    naturalHeight: number;
-  }) => void;
 }
 
 /**
