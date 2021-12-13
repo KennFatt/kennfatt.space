1509228cd2b45a7ef2597ee6dcd3ee5b256315e5
Kennan Fattahillah <10755895+KennFatt@users.noreply.github.com>
2021-12-14 00:39:29 +0700

    Oops, not just yet...

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,23 +1,11 @@
-import { SectionHead } from "components/base/SectionHead";
-import { Section } from "components/blocks/Section";
 import { PageBaseLayout } from "components/layout/PageBaseLayout";
 
 import type { NextPageWithLayout } from "lib/common-types";
 
 const AboutPage: NextPageWithLayout = () => {
-  return (
-    <Section hasVerticalPadding centerContent>
-      <SectionHead
-        title="How i can help"
-        description="Culpa cillum deserunt mollit cillum dolor Lorem et aliqua."
-      />
-      <p>Reprehenderit ipsum consequat exercitation nisi.</p>
-    </Section>
-  );
+  return <></>;
 };
 
-AboutPage.getLayout = (page) => (
-  <PageBaseLayout pageName="About">{page}</PageBaseLayout>
-);
+AboutPage.getLayout = (_) => <PageBaseLayout pageName="About" />;
 
 export default AboutPage;
