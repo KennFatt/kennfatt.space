f9981c847f4591609c49673971b902e1c53561af
Kennan Fattahillah <kennfatt@gmail.com>
2022-03-27 12:59:35 +0700

    feat: add Sentry integration.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
-const withBundleAnalyzer = require("@next/bundle-analyzer")({
-  enabled: process.env.ANALYZE === "true",
-});
+const withPlugins = require("next-compose-plugins");
+const { withSentryConfig } = require("@sentry/nextjs");
+const withBundleAnalyzer = require("@next/bundle-analyzer");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -18,4 +18,30 @@ const nextConfig = {
   },
 };
 
-module.exports = withBundleAnalyzer(nextConfig);
+// module.exports = withBundleAnalyzer(nextConfig);
+
+module.exports = withPlugins(
+  [
+    [
+      withBundleAnalyzer,
+      {
+        enabled: process.env.ANALYZE === "true",
+      },
+    ],
+    [
+      withSentryConfig,
+      {
+        // Additional config options for the Sentry Webpack plugin. Keep in mind that
+        // the following options are set automatically, and overriding them is not
+        // recommended:
+        //   release, url, org, project, authToken, configFile, stripPrefix,
+        //   urlPrefix, include, ignore
+
+        silent: true, // Suppresses all logs
+        // For all available options, see:
+        // https://github.com/getsentry/sentry-webpack-plugin#options.
+      },
+    ],
+  ],
+  nextConfig
+);
