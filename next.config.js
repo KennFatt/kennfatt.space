f0cc75d36b2de2b817d5846be0aa16ee6c857bf5
Kennan Fattahillah <kennfatt@gmail.com>
2022-05-04 20:47:03 +0700

    chore(`sentry`): temporary ditch Sentry from the app.
    - The main reason is now I'm migrating to CF for issue and error tracking.

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 const withPlugins = require("next-compose-plugins");
-const { withSentryConfig } = require("@sentry/nextjs");
+// const { withSentryConfig } = require("@sentry/nextjs");
 const withBundleAnalyzer = require("@next/bundle-analyzer");
 
 /** @type {import('next').NextConfig} */
@@ -26,20 +26,20 @@ module.exports = withPlugins(
         enabled: process.env.ANALYZE === "true",
       },
     ],
-    [
-      withSentryConfig,
-      {
-        // Additional config options for the Sentry Webpack plugin. Keep in mind that
-        // the following options are set automatically, and overriding them is not
-        // recommended:
-        //   release, url, org, project, authToken, configFile, stripPrefix,
-        //   urlPrefix, include, ignore
+    // [
+    //   withSentryConfig,
+    //   {
+    //     // Additional config options for the Sentry Webpack plugin. Keep in mind that
+    //     // the following options are set automatically, and overriding them is not
+    //     // recommended:
+    //     //   release, url, org, project, authToken, configFile, stripPrefix,
+    //     //   urlPrefix, include, ignore
 
-        silent: true, // Suppresses all logs
-        // For all available options, see:
-        // https://github.com/getsentry/sentry-webpack-plugin#options.
-      },
-    ],
+    //     silent: true, // Suppresses all logs
+    //     // For all available options, see:
+    //     // https://github.com/getsentry/sentry-webpack-plugin#options.
+    //   },
+    // ],
   ],
   nextConfig
 );
