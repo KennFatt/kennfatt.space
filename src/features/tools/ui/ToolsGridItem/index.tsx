a2aad544b103a5a8da297e693170df312bf12ff7
Kennan Fattahillah <kennfatt@gmail.com>
2025-03-02 19:15:52 +0700

    ref(features/tools): use `<Link>` instead of anchor element in `<ToolsGridItemProps>` component
    - Updated `tailwind.config.js` scan dir

--- src/features/tools/ui/ToolsGridItem/index.tsx
+++ src/features/tools/ui/ToolsGridItem/index.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import type { FC } from "react";
 
 export interface ToolsGridItemProps {
@@ -20,17 +21,16 @@ export const ToolsGridItem: FC<ToolsGridItemProps> = ({
   href,
 }) => {
   return (
-    <a
-      href={href}
-      className="group transition-colors rounded-lg p-2 border-2 bg-base-light-dim dark:bg-base-dark-dim dark:border-base-dark-dim border-base-light-dim space-y-4"
-    >
-      {/* Title */}
-      <h3 className="group-hover:text-primary-light transition-colors font-serif text-xl lg:text-2xl font-medium">
-        {title}
-      </h3>
+    <Link href={href}>
+      <a className="group transition-colors rounded-lg p-2 border-2 bg-base-light-dim dark:bg-base-dark-dim dark:border-base-dark-dim border-base-light-dim space-y-4">
+        {/* Title */}
+        <h3 className="group-hover:text-primary-light transition-colors font-serif text-xl lg:text-2xl font-medium">
+          {title}
+        </h3>
 
-      {/* Description */}
-      {description ? <p>{description}</p> : <>&nbsp;</>}
-    </a>
+        {/* Description */}
+        {description ? <p>{description}</p> : <>&nbsp;</>}
+      </a>
+    </Link>
   );
 };
